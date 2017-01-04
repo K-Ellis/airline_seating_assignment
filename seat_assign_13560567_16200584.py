fce287875a0f2b8e8dd580b3d4cd2bfbaa9db1e9
Kieron Ellis <kieron.ellis@ucdconnect.ie>
2017-01-04 23:24:22 +0000

    update seat_assign.py and create a test file where I was messing around with sql and looking at what's in the database

--- seat_assign_13560567_16200584.py
+++ seat_assign_13560567_16200584.py
@@ -1,57 +1,34 @@
-import numpy as np
 import sqlite3
-import sys
+import numpy as np
+# todo-kieron turn a sql database into an array
 
-#Connect to the database using sqlite2's .connect() method which returns a
-#connection object
-conn = sqlite3.connect("airline_seating.db")
-#From the connection we get a cursor object
-cur = conn.cursor()
+def sql_to_array(database):
+    # Connect to the database using sqlite2's .connect() method which returns a
+    # connection object
+    conn = sqlite3.connect(database)
+    # From the connection we get a cursor object
+    cur = conn.cursor()
+    with conn:
+        cur.execute("SELECT * FROM rows_cols")
 
-#Prints all of the tables
-with conn:
-    #Tables names are stored in sqlite_master
-    cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
+        rowscols = cur.fetchone()
+        no_rows = rowscols[0]
+        col_letters = rowscols[1]
 
-    list_of_tables = cur.fetchall()
-
-    for table1 in list_of_tables:
-        print(table1[0])
-#The tables stored in airline_seating.db are: metrics, row_cols and seating
-
-print()
-
-#print out the info in metrics
-with conn:
-    cur.execute("SELECT * FROM metrics")
-
-    rows = cur.fetchall()
-
-    for row in rows:
-        print(row)
-
-print()
-
-# print out the info in rows_cols
-with conn:
-    cur.execute("SELECT * FROM rows_cols")
-
-    rows = cur.fetchall()
-
-    for row in rows:
-        print(row)
-
-print()
-
-# print out the info in seating
-with conn:
-    cur.execute("SELECT * FROM seating")
-
-    rows = cur.fetchall()
-
-    for row in rows:
-        print(row)
+    # release the resources
+    no_cols = 0
+    for letter in col_letters:
+        no_cols += 1
+    row_col_matrix = np.zeros((no_rows, no_cols))
 
 
-#release the resources
-conn.close()
+    conn.close()
+    #want to return a 1s and zeros matrix
+    return row_col_matrix
+row_col_matrix = sql_to_array("airline_seating.db")
+print(row_col_matrix.shape)
+
+
+# todo-remi solve seating
+
+
---
+++ test_file.py
@@ -0,0 +1,54 @@
+import sqlite3
+#Connect to the database using sqlite2's .connect() method which returns a
+#connection object
+conn = sqlite3.connect("airline_seating.db")
+#From the connection we get a cursor object
+cur = conn.cursor()
+
+#Prints all of the tables
+with conn:
+    #Tables names are stored in sqlite_master
+    cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
+
+    list_of_tables = cur.fetchall()
+
+    for table1 in list_of_tables:
+        print(table1[0])
+#The tables stored in airline_seating.db are: metrics, row_cols and seating
+
+print()
+
+#print out the info in metrics
+with conn:
+    cur.execute("SELECT * FROM metrics")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+print()
+
+# print out the info in rows_cols
+with conn:
+    cur.execute("SELECT * FROM rows_cols")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+print()
+
+# print out the info in seating
+with conn:
+    cur.execute("SELECT * FROM seating")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+
+
+conn.close()
