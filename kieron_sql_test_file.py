d0b74c98d28fa2f11d402ad140711e85140411f7
Kieron Ellis <kieron.ellis@ucdconnect.ie>
2017-01-05 00:08:58 +0000

    create function to make a zeros array the same dimension as the seating plan. create a second function to fill in that array with 1s where the seats are occupied

--- kieron_sql_test_file.py
+++ kieron_sql_test_file.py
@@ -5,50 +5,55 @@ conn = sqlite3.connect("airline_seating.db")
 #From the connection we get a cursor object
 cur = conn.cursor()
 
-#Prints all of the tables
-with conn:
-    #Tables names are stored in sqlite_master
-    cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
+def print_entries(name):
+    # Prints all of the tables
+    if name == "tables":
+        with conn:
+            #Tables names are stored in sqlite_master
+            cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
 
-    list_of_tables = cur.fetchall()
+            list_of_tables = cur.fetchall()
 
-    for table1 in list_of_tables:
-        print(table1[0])
-#The tables stored in airline_seating.db are: metrics, row_cols and seating
+            for table1 in list_of_tables:
+                print(table1[0])
+        #The tables stored in airline_seating.db are: metrics, row_cols and seating
+    #print out the info in metrics
+    elif name == "metrics":
+        with conn:
+            cur.execute("SELECT * FROM metrics")
 
-print()
+            rows = cur.fetchall()
 
-#print out the info in metrics
-with conn:
-    cur.execute("SELECT * FROM metrics")
+            for row in rows:
+                print(row)
+    # print out the info in rows_cols
+    elif name == "rows_cols":
+        with conn:
+            cur.execute("SELECT * FROM rows_cols")
 
-    rows = cur.fetchall()
+            rows = cur.fetchall()
 
-    for row in rows:
-        print(row)
+            for row in rows:
+                print(row)
+    else:
+        # print out the info in seating
+        with conn:
+            cur.execute("SELECT * FROM seating")
 
-print()
+            rows = cur.fetchall()
 
-# print out the info in rows_cols
-with conn:
-    cur.execute("SELECT * FROM rows_cols")
-
-    rows = cur.fetchall()
-
-    for row in rows:
-        print(row)
-
-print()
-
-# print out the info in seating
-with conn:
-    cur.execute("SELECT * FROM seating")
-
-    rows = cur.fetchall()
-
-    for row in rows:
-        print(row)
-
-
+            # for row in rows:
+            #     print(row)
+            print(len(rows))
+            print(rows)
+print_entries("seating")
+#60 entries, two seats taken
+# 1 A Donald Trump and 1 C Hilary Clinton
+print_entries("rows_cols")
+# 15*4
+# A C D F
+# 0 1 2 4
+print_entries("metrics")
+print_entries("tables")
 
 conn.close()
--- seat_assign_13560567_16200584.py
+++ seat_assign_13560567_16200584.py
@@ -1,34 +1,71 @@
 import sqlite3
 import numpy as np
-# todo-kieron turn a sql database into an array
+#todo-kieron get seating database name and csv name from the command line.
+seating_database = "airline_seating.db"
 
-def sql_to_array(database):
-    # Connect to the database using sqlite2's .connect() method which returns a
-    # connection object
-    conn = sqlite3.connect(database)
-    # From the connection we get a cursor object
-    cur = conn.cursor()
+# Connect to the database using sqlite2's .connect() method which returns a
+# connection object.
+conn = sqlite3.connect(seating_database)
+# From the connection we get a cursor object.
+cur = conn.cursor()
+
+def create_zeros_array():
     with conn:
         cur.execute("SELECT * FROM rows_cols")
 
         rowscols = cur.fetchone()
         no_rows = rowscols[0]
         col_letters = rowscols[1]
-
-    # release the resources
     no_cols = 0
+    col_letters_list =[]
     for letter in col_letters:
         no_cols += 1
+        col_letters_list.append(letter)
     row_col_matrix = np.zeros((no_rows, no_cols))
 
-
-    conn.close()
     #want to return a 1s and zeros matrix
-    return row_col_matrix
-row_col_matrix = sql_to_array("airline_seating.db")
-print(row_col_matrix.shape)
+    return row_col_matrix, col_letters_list
+
+zeros_array, col_letters_list = create_zeros_array()
+# print(zeros_array.shape)
 
 
-# todo-remi solve seating
+# todo-kieron fill in the 1s from the database file. Turn the ZerosMatrix into a
+# ones_and_zeros matrix.
+
+def find_occupied():
+    with conn:
+        cur.execute("SELECT * FROM seating")
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            if row[2]== "":
+                pass
+            else:
+                for letter in col_letters_list:
+                    if row[1] == letter:
+                        # print(letter, col_letters_list.index(letter))
+                        zeros_array[row[0]-1][col_letters_list.index(letter)] \
+                                                                        = 1
+                        # print(row[0])
+                        # print(row[1])
+                        # print(row[2])
+find_occupied()
+print(zeros_array)
+
+# todo-kieron change the database file when a booking has been made. Using the
+# ones_and_zeros matrix and booking name.
+
+# todo-remi solve seating.
+# When a booking is made, have to store the entries where the the seats are
+# allocated so that the database can also be updated. Could create a new
+# discardable zeros-only matrix, and update it with 1s too, and use it to update
+# the sql database.
+# Or store the booking name, row, and column letter in a separate list... and
+# append the new bookings onto the end of the list. And use this list to update
+# the database. I think this is probably a better idea.
 
 
+# release the resources
+conn.close()
